ecd67de1c2d59ef4dd3a4e5f349a4650611f8157
Natalie Vu <tnvu7@myseneca.ca>
2021-11-15 21:13:42 -0500

    Added getUserLocation. Where to put it?

--- capstone-rn-blank-js-app/App.js
+++ capstone-rn-blank-js-app/App.js
@@ -1,8 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { render } from 'react-dom';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  getUserLocation() {
+    if (navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(position => console.log(position));
+    } else {
+      console.log("cannot find location");
+    }
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
