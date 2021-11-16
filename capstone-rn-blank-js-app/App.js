c6d4af385a7bc92fa9c6ca070de676a6dd8cd756
Justin Kim <mletemps@hotmail.com>
2021-11-15 22:31:23 -0500

    created the string that will make the api call and display it on click

--- capstone-rn-blank-js-app/App.js
+++ capstone-rn-blank-js-app/App.js
@@ -6,12 +6,16 @@ import * as Location from 'expo-location';
 export default function App() {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
-  const [baseURL, setBaseURL] = useState("api.tomtom.com");
+  // const [coordinates, setCoordinates] = useState([longitude: "",latitude: ""]);
+  const [longitude, setLongitude] = useState("");
+  const [latitude, setLatitude] = useState("");
+  const [baseURL, setBaseURL] = useState("https://api.tomtom.com");
   const [versionNumber, setVersionNumber] = useState("2");
   const [responseFormat, setResponseFormat] = useState("json");
   const [apiKey, setApiKey] = useState("zGpy7bbwejkRKMGFfZMhGG4FCpR6IgKV");
   const [radius, setRadius] = useState("100");
   const [fullURL, setFullURL] = useState("");
+  const [query, setQuery] = useState("");
 
 
   useEffect(() => {
@@ -31,13 +35,24 @@ export default function App() {
   if (errorMsg) {
     text = errorMsg;
   } else if (location) {
-    text = JSON.stringify(location);
+    // text = JSON.stringify(location);
+    text = fullURL;
      
   }
 
   function callAPI()
   {
     console.log("nice");
+    setLongitude(prev => location.coords.longitude);
+    setLatitude(prev => location.coords.latitude);
+    setQuery(prev => "pizza");
+    setFullURL(prev =>  baseURL + "/search/" 
+                        + versionNumber + "/poiSearch/" 
+                        + query + "."+ responseFormat 
+                        + "?key=" + apiKey 
+                        + "&radius=" + radius 
+                        + "&long=" + longitude
+                        + "&lat=" + latitude);
   }
 
   return (
