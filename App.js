851a3106a2f52576fb8bfb005ebad225de908335
Tue <tnguyen187@myseneca.ca>
2021-11-17 02:28:07 -0500

    Removed unnecessary state

--- App.js
+++ App.js
@@ -16,13 +16,12 @@ const App = () => {
   const [events, setEvents] = useState([]);
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
-  const [baseURL] = useState("https://api.tomtom.com");
-  const [versionNumber] = useState("2");
-  const [responseFormat] = useState("json");
-  const [apiKey] = useState("YlHRCAWHnD2YVU8wWQqwfJA5VRKpHOPm");
-  const [query, setQuery] = useState();
-
   const [response, setResponse] = useState();
+
+  const baseURL = "https://api.tomtom.com";
+  const versionNumber = "2";
+  const responseFormat = "json";
+  const apiKey = "YlHRCAWHnD2YVU8wWQqwfJA5VRKpHOPm";
 
   useEffect(() => {
     getAllEvents(setEvents);
@@ -34,7 +33,6 @@ const App = () => {
       }
       let location = await Location.getCurrentPositionAsync({});
       setLocation(location);
-      setQuery("pizza");
     })();
   }, []);
 
@@ -90,21 +88,25 @@ const App = () => {
 
   return (
     <View style={styles.container}>
-      <View>
-        <form onSubmit={formik.handleSubmit}>
-          <input
-            id="location"
-            name="location"
-            type="text"
-            onChange={formik.handleChange}
-            value={formik.values.location}
-          />
-          <button>Search</button>
-        </form>
-      </View>
-      {response != null ? (
+      {response == null ? (
         <View>
-          <Text>Restaurants nearby for {query}:</Text>
+          {text == null ? (
+            <form onSubmit={formik.handleSubmit}>
+              <input
+                id="location"
+                name="location"
+                type="text"
+                onChange={formik.handleChange}
+                value={formik.values.location}
+              />
+              <button>Search</button>
+            </form>
+          ) : (
+            <Text style={styles.paragraph}>{text}</Text>
+          )}
+        </View>
+      ) : (
+        <View>
           <Text style={styles.paragraph}>
             {response.results[0].poi.name},{" "}
             {response.results[0].address.streetNumber} &nbsp;
@@ -127,8 +129,6 @@ const App = () => {
             {response.results[2].address.extendedPostalCode}
           </Text>
         </View>
-      ) : (
-        <Text style={styles.paragraph}>{text}</Text>
       )}
       {events.length > 0 ? (
         <Text>{events[0].event_name}</Text>
