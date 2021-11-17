d27b16d05f1c1215e16f7bd86f0f4fbddb6524c0
Natalie Vu <tnvu7@myseneca.ca>
2021-11-17 00:11:05 -0500

    Add display functionality

--- capstone-rn-blank-js-app/App.js
+++ capstone-rn-blank-js-app/App.js
@@ -3,6 +3,9 @@ import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 import * as Location from "expo-location";
 import { render } from "react-dom";
+import t from 'tcomb-form-native';
+
+const Form = t.form.Form;
 
 export default function App() {
   const [location, setLocation] = useState(null);
@@ -10,10 +13,10 @@ export default function App() {
   const [baseURL, setBaseURL] = useState("https://api.tomtom.com");
   const [versionNumber, setVersionNumber] = useState("2");
   const [responseFormat, setResponseFormat] = useState("json");
-  const [apiKey, setApiKey] = useState("zGpy7bbwejkRKMGFfZMhGG4FCpR6IgKV");
+  const [apiKey, setApiKey] = useState("YlHRCAWHnD2YVU8wWQqwfJA5VRKpHOPm");
   const [radius, setRadius] = useState("100");
   const [query, setQuery] = useState("pizza");
-  const [response, setResponse] = useState();
+  const [response, setResponse] = useState(null);
 
   useEffect(() => {
     (async () => {
@@ -32,20 +35,26 @@ export default function App() {
   if (errorMsg) {
     text = errorMsg;
   } else if (location) {
-    text = JSON.stringify(response);
+    //text = JSON.stringify(response);
+    text="Done loading";
   }
 
-  async function callAPI() {
+  function callAPI() {
     try {
       let url =
-        baseURL + "/search/" + versionNumber + "/poiSearch/" + query +
-        "." + responseFormat + "?key=" + apiKey + "&radius=" + radius +
-        "&long=" + location.coords.latitude + "&lat=" + location.coords.longitude;
+        baseURL + "/search/" + versionNumber + "/search/" + query +
+        "." + responseFormat + "?lat=" + location.coords.latitude + 
+        "&lon=" + location.coords.longitude + "&limit=3" + "&key=" + apiKey;
 
-      console.log("lat:" + location.coords.latitude + ",lon:" + location.coords.longitude + ",query:" + query);
-      console.log("apiurl:" + url);
+      console.log("lat: " + location.coords.latitude + ", lon: " + location.coords.longitude + ", query: " + query);
+      console.log("apiurl: " + url);
 
-      const res = await fetch(url).then((res) => res.json()).then(data=>setResponse(data.results))
+      const res = fetch(url).then((res) => res.json()).then(data=>{
+        setResponse(data);
+        console.log(response);
+        console.log("Response is type: " + typeof(response));
+        console.log(response.results[0].poi.name);
+      });
 
     } catch (err) {
       console.log("ERROR: " + err);
@@ -54,7 +63,19 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Button title="Call API" onPress={callAPI} />
-      <Text style={styles.paragraph}>{text}</Text>
+      
+      {response != null ? 
+      <View>
+        <Text>Restaurants nearby for {query}:</Text>
+        <Text style={styles.paragraph}>{response.results[0].poi.name}, {response.results[0].address.streetNumber} &nbsp; 
+        {response.results[0].address.streetName}, {response.results[0].address.municipality}, {response.results[0].address.extendedPostalCode}</Text> 
+        <Text style={styles.paragraph}>{response.results[1].poi.name}, {response.results[1].address.streetNumber} &nbsp; 
+        {response.results[1].address.streetName}, {response.results[1].address.municipality}, {response.results[1].address.extendedPostalCode} </Text>
+        <Text style={styles.paragraph}>{response.results[2].poi.name}, {response.results[2].address.streetNumber} &nbsp; 
+        {response.results[2].address.streetName}, {response.results[2].address.municipality}, {response.results[2].address.extendedPostalCode}</Text>
+        </View>
+      : <Text style={styles.paragraph}>{text}</Text>}
+      
     </View>
   );
 }
