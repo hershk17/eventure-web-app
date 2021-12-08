80e0fe656ed342833da38b4f425d2ee9a70638dc
Tue Nguyen <tnguyen187@myseneca.ca>
2021-12-08 03:47:17 -0500

    Added subscribe to route queryParams

--- src/app/pages/verify-email/verify-email.page.ts
+++ src/app/pages/verify-email/verify-email.page.ts
@@ -1,17 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { DbService } from '../../services/db.service';
-
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-verify-email',
   templateUrl: './verify-email.page.html',
   styleUrls: ['./verify-email.page.scss'],
 })
 export class VerifyEmailPage implements OnInit {
-  public msg = 'Please check your email and click on the link to verify your email address!';
-  constructor(public db: DbService, private route: ActivatedRoute) {
-    this.msg = this.route.snapshot.queryParamMap.get('msg');
+  public msg = '';
+  constructor(public db: DbService, private route: ActivatedRoute, private location: Location, private router: Router) {
+    this.route.queryParams.subscribe(params => {
+      console.log(params['msg']);
+      if(params['msg']) {
+        this.msg = params['msg'];
+      }
+      else {
+        this.msg = 'Please check your email and click on the link to verify your email address!';
+      }
+    });
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 }
