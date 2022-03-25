c164798bf8ea5c861349db9dd95f3f655d1a8ea1
tnvu7 <tnvu7@myseneca.ca>
2022-03-25 00:02:33 -0400

    Format Timeline

--- src/app/pages/home-timeline/home-timeline.page.ts
+++ src/app/pages/home-timeline/home-timeline.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { DbService, Post } from 'src/app/services/db.service';
+import {
+  DbService,
+  ImagePost,
+  Post,
+  TextPost,
+} from 'src/app/services/db.service';
 
 @Component({
   selector: 'app-home-timeline',
@@ -7,10 +12,9 @@ import { DbService, Post } from 'src/app/services/db.service';
   styleUrls: ['./home-timeline.page.scss'],
 })
 export class HomeTimelinePage implements OnInit {
-
   posts: Post[] = [];
 
-  constructor(private db: DbService) { }
+  constructor(private db: DbService) {}
 
   ngOnInit() {
     this.db.getPostsFromFollowingList().then((res) => {
@@ -22,6 +26,7 @@ export class HomeTimelinePage implements OnInit {
   doRefresh(event) {
     this.db.getPostsFromFollowingList().then((res) => {
       this.posts = res;
+
       console.log(this.posts);
     });
 
@@ -29,5 +34,33 @@ export class HomeTimelinePage implements OnInit {
       console.log('Async operation has ended');
       event.target.complete();
     }, 2000);
+  }
+
+  toDate(timestamp) {
+    let date = new Date(timestamp);
+    let ampm = date.getHours() >= 12 ? 'PM' : 'AM';
+    let hours = date.getHours() % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    let minutes =
+      date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
+    var strTime =
+      date.getDate() +
+      '/' +
+      (date.getMonth() + 1) +
+      '/' +
+      date.getFullYear() +
+      ' ' +
+      hours +
+      ':' +
+      minutes +
+      ' ' +
+      ampm;
+    return strTime;
+  }
+  isMyPost(uid){
+    return uid == this.db.userData.uid;
+  }
+  deletePost(uid){
+    
   }
 }
